c2254083ed56916742a0d9c1d36fd7ff09ba34c7
davidfrp <davidfrp@outlook.dk>
2021-12-01 14:51:52 +0100

    Added Location class.

---
+++ src/main/java/com/example/demo/entities/Location.java
@@ -0,0 +1,34 @@
+package com.example.demo.entities;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Location {
+
+    private double latitude;
+
+    private double longitude;
+
+    protected Location() { }
+
+    public Location(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+}
