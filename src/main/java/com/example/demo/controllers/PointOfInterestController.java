c0ab731b7418c8fcd7d36013335f80968c33cc2a
davidfrp <davidfrp@outlook.dk>
2021-12-02 10:37:13 +0100

    Added pagination for PoI endpoint.

--- src/main/java/com/example/demo/controllers/PointOfInterestController.java
+++ src/main/java/com/example/demo/controllers/PointOfInterestController.java
@@ -2,11 +2,15 @@ package com.example.demo.controllers;
 
 import com.example.demo.entities.PointOfInterest;
 import com.example.demo.repositories.PointOfInterestRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @Controller
@@ -21,6 +25,17 @@ public class PointOfInterestController {
     @GetMapping("/points-of-interest")
     public ResponseEntity<List<PointOfInterest>> findAll() {
         return ResponseEntity.ok().body(pointOfInterestRepository.findAll());
+    }
+
+    @GetMapping("/points-of-interest")
+    public ResponseEntity<List<PointOfInterest>> findAll(
+            @PathParam("page") int page,
+            @PathParam("perPage") int perPage) {
+
+        Page<PointOfInterest> pointsOfInterestPage =
+                pointOfInterestRepository.findAll(PageRequest.of(page - 1, perPage));
+
+        return ResponseEntity.ok().body(pointsOfInterestPage.getContent());
     }
 
     @GetMapping("/points-of-interest/{id}")
