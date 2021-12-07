84a5f352aaec67b3477e5e91a95112c00354b7ea
davidfrp <davidfrp@outlook.dk>
2021-12-07 14:27:40 +0100

    Allowed all origin for CORS.

--- src/main/java/com/example/demo/controllers/PointOfInterestController.java
+++ src/main/java/com/example/demo/controllers/PointOfInterestController.java
@@ -5,6 +5,7 @@ import com.example.demo.entities.PointOfInterest;
 import com.example.demo.repositories.PointOfInterestRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -13,6 +14,7 @@ import java.util.Optional;
 import java.util.List;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class PointOfInterestController {
 
     private final PointOfInterestRepository pointOfInterestRepository;
--- src/main/java/com/example/demo/controllers/SpeciesController.java
+++ src/main/java/com/example/demo/controllers/SpeciesController.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class SpeciesController {
 
     @Autowired
