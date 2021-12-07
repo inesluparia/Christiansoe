10ee64ffd51ec97302018e9f1f27e1994e5012a4
davidfrp <davidfrp@outlook.dk>
2021-12-07 12:17:04 +0100

    Fixed fuckup, idk what I did.

--- website/src/pages/animals/animalsPage.js
+++ website/src/pages/animals/animalsPage.js
@@ -1,16 +1,6 @@
-<<<<<<< HEAD:website/src/pages/animals/animalsPage.js
 function AnimalsPage(props) {
     
     const html = props.animals.map(animal => `
-=======
-function AnimalsPage() {
-
-    // const animalsResponse = await fetch("http://localhost:8080/animals")
-    //     .then(res => res.json());
-    const animalsResponse = [];
-
-    const html = animalsResponse.map(animal => `
->>>>>>> 84959002b009dcc05024a9b76b365af460074a98:website/src/pages/animals/animals.js
         <li>
             <a href="/species/${animal.id}" data-navigo>
                 ${animal.name}, ${animal.latinName}
