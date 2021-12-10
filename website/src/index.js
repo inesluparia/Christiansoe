33af7cf717a690b9f41ad2ce147db1ab097a5a12
davidfrp <davidfrp@outlook.dk>
2021-12-10 21:16:03 +0100

    Fixed points of interest filtering.

--- website/src/index.js
+++ website/src/index.js
@@ -5,6 +5,7 @@ import PointsOfInterestPage from "./pages/points-of-interest/pointsOfInterestPag
 import AnimalsPage from "./pages/animals/animalsPage";
 import { pointsOfInterestService } from "./services/pointsOfInterestService";
 import { speciesService } from "./services/speciesService";
+import { locationService } from "./services/locationService";
 
 const router = new Navigo("/");
 const rootElement = document.getElementById("root");
@@ -19,14 +20,37 @@ router.on({
     "/points-of-interest": async () => {
         const pointsOfInterest = await pointsOfInterestService.findAll();
 
-        const onFilterChange = (filter) => {
-            alert(filter);
+        const renderPointsOfInterestPage = (props) => {
+            renderPageElement(
+                PointsOfInterestPage(props),
+                rootElement
+            );
         };
 
-        renderPageElement(
-            PointsOfInterestPage({ pointsOfInterest, onFilterChange }),
-            rootElement
-        );
+        const onFilterChange = (sortBy) => {
+            if (sortBy === "distance") {
+                locationService.getCurrentLocationAsync().then(currentLocation => {
+                    // Adding, as a property, on each point of interest, the distance from 
+                    // the user's current location to the location of the point of interest.
+                    Promise.all(pointsOfInterest.map(async (pointOfInterest) => {
+                        const distance = await locationService
+                            .getDistanceBetween(currentLocation, pointOfInterest.location);
+    
+                        pointOfInterest.distance = distance;
+    
+                        return pointOfInterest;
+    
+                    })).then(() => {
+                        renderPointsOfInterestPage({ pointsOfInterest, onFilterChange, sortBy });
+                    });
+                });
+
+            } else {
+                renderPointsOfInterestPage({ pointsOfInterest, onFilterChange, sortBy });
+            }
+        };
+
+        renderPointsOfInterestPage({ pointsOfInterest, onFilterChange, sortBy: "name" });
     },
 })
 .notFound(() => {
