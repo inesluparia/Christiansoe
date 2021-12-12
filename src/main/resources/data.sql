a01141a93c121f52500b65c6e6ab8d3960128dd7
davidfrp <davidfrp@outlook.dk>
2021-12-12 15:22:13 +0100

    Trying to fix merge conflicts.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,30 +6,32 @@ INSERT INTO species (latin_name, name, description, is_animal, discoverable_wint
     ('Armeria maritima', 'Engelsk græs', 'En flerårig plante, der ved grunden har en roset af 5-10 cm. lange linjeformede, en smule sukkulente blade med but afrundet spids. Blomsterskafterne er bladløse og indtil 40 cm. høje og fra glatte til dunhårede-hårede. Blomsterstanden er hovedformet og omgivet af hindeagtige svøbblade, hvor de ydre har nedadrettede forlængelser, der omslutter blomsterskaftets øvre del - og vækstzonen, der befinder sig lige under blomsterstanden. Blomsterne er vellugtende og rosenrøde til hvide.',0, 0, 1, 1, 1),
     ('Somateria mollissima', 'Ederfugl', 'Ederfuglen er en af vores mest talrige havdykænder. Det er en stor og kraftig dykand, 60-70 cm lang og med et vingefang på ca. 1 meter. Den har et meget karakteristisk langt trekantet næb, der gør den nem at kende i silhuet. Hannen (adult) er utrolig flot i pragtdragt med hvid hals og ryg, mens flanker, mave og hale er sort. Hovedet er hvidt med sort isse og i kontrast er nakkefjerene olivengrønne. Eclipsedragten, som hannen har under fjerfældningen i juli-september, er mere brunlig/spættet med spredte hvide områder på kroppen. Det tager ca. 3 år før hannen har opnået sin pragtdragt. Indtil da er fjerdragten meget broget i sorte, hvide og brune nuancer. Hunnen er brunlig og med en lys næbspids. Juvenile hunner har svage aftegninger og lys øjenbrynsstribe.', 1, 1, 1, 1, 1),
     ('Tulipa sylvestris', 'Vild tulipan','Vild tulipan er naturligt udbredt i Middelhavsområdet med tyngdepunktet i Tyrkiet, Grækenland og på Sicilien. Senere er den spredt og naturaliseret fra dyrkning over store dele af Europa. Den findes voksende enkelte steder i Danmark, hvor den er forvildet fra have og park. Arten foretrækker lysåbne voksesteder i krat, langs skovbryn og hække og i vinmarker, hvor jorden er leret og kalkrig.', 0,0,1,1,0),
-    ('Phoca vitulina', 'Spættet sæl', 'Den spættede sæl er den eneste sælart der med sikkerhed yngler i Danmark. Parringstid i juni-juli. Drægtighed 11 mdr. Føder en unge. Kønsmodne ved ca 130 cm, svarende til 3-5 år. Levetid op til 36 år. Den spættede sæl er ekstremt tilpasset et liv i vand. Den kan ikke, som f.eks. søløver, bruge lufferne på land, men må "mave" sig langsomt frem. Derfor holder den sig tæt på vandet og ses sjældent mere end et par meter fra kysten. I vandet kan den skyde en fart af op til 35 km/t, dykke til noget over 100 m og holde vejret i 20-30 min. Kommer oftest op efter luft med 5-10 min. intervaller. Spættede sæler lever i flokke af varierende størrelse uden hakkeorden eller en egentlig social struktur. Lever af fisk og krebsdyr.', 1, 1, 1, 1, 1)
-    ;
+    ('Phoca vitulina', 'Spættet sæl', 'Den spættede sæl er den eneste sælart der med sikkerhed yngler i Danmark. Parringstid i juni-juli. Drægtighed 11 mdr. Føder en unge. Kønsmodne ved ca 130 cm, svarende til 3-5 år. Levetid op til 36 år. Den spættede sæl er ekstremt tilpasset et liv i vand. Den kan ikke, som f.eks. søløver, bruge lufferne på land, men må "mave" sig langsomt frem. Derfor holder den sig tæt på vandet og ses sjældent mere end et par meter fra kysten. I vandet kan den skyde en fart af op til 35 km/t, dykke til noget over 100 m og holde vejret i 20-30 min. Kommer oftest op efter luft med 5-10 min. intervaller. Spættede sæler lever i flokke af varierende størrelse uden hakkeorden eller en egentlig social struktur. Lever af fisk og krebsdyr.', 1, 1, 1, 1, 1);
 
 INSERT INTO media (url, is_image) VALUES
     ('/images/Pelophylax esculentus.jpg', true),
-    ('/images/Honningbi.jpg', true),
-    ('/sounds/Cyanistes caeruleus.mp3', false),
+    ('/images/Apis mellifera.jpg', true),
     ('/images/Cyanistes caeruleus.jpg', true),
     ('/images/Cichorium intybus.jpg', true),
     ('/images/Armeria maritima.jpg', true),
     ('/images/Somateria mollissima.jpg', true),
+    ('/images/Tulipa sylvestris.jpg', true),
+    ('/images/Phoca vitulina.jpg', true),
     ('/sounds/Somateria mollissima.mp3', false),
-    ('/images/Tulipa sylvestris.jpg', false),
+    ('/sounds/Cyanistes caeruleus.mp3', false),
     ('/sounds/Phoca vitulina.mp3', false),
-    ('/images/Phoca vitulina.jpg', true)
-    ;
+    ('/images/Kongens bastion.jpg', true);
 
 INSERT INTO species_media (species_id, media_id) VALUES
-     (1, 1), (2, 2), (3, 3), (3, 4), (4, 5), (5, 6), (6, 7), (6, 8), (7, 9), (8, 10), (8, 11);
+    (1, 1), (2, 2), (3, 3), (4, 4), (5, 5), (6, 6), (7, 7), (8, 8), (6, 9), (3, 10), (8, 11);
 
 INSERT INTO points_of_interest (description, latitude, longitude, name) VALUES
-    ('description will come.', 55.3192258, 15.1890070,'Møllebakken på Christiansø'),
-    ('description will come.', 55.3179662, 15.1883479, 'Kongens Bastion'),
-    ('description will come.', 55.3232303, 15.1910652, 'Verdens ende');
+    ('No description.', 55.3192258, 15.1890070, 'Møllebakken på Christiansø'),
+    ('No description.', 55.3179662, 15.1883479, 'Kongens Bastion'),
+    ('No description.', 55.3232303, 15.1910652, 'Verdens ende');
+
+INSERT INTO points_of_interest_media (point_of_interest_id, media_id) VALUES
+    (2, 12);
 
 INSERT INTO points_of_interest_species (points_of_interest_id, species_id) VALUES
     (1, 1), (1, 2), (2, 1), (2, 3), (2, 4), (3, 3);
