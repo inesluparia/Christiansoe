35081e191e1d6a132cae9fcc92888fa7ad9cd98b
davidfrp <davidfrp@outlook.dk>
2021-12-15 14:17:29 +0100

    Changed some styling.

--- website/src/pages/navigationPage.js
+++ website/src/pages/navigationPage.js
@@ -21,14 +21,16 @@ function NavigationPage() {
                     <a href="/plants" data-navigo>Planter</a>
                 </li>
             </ul>
-            <div class="ferry-info">
+            <div class="ferry-info" title="Afstand og tid til færgen.">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <circle cx="12" cy="5" r="3"></circle>
                     <line x1="12" y1="22" x2="12" y2="8"></line>
                     <path d="M5 12H2a10 10 0 0020 0h-3"></path>
                 </svg>
-                <span class="ferry-distance">~485 m</span>
-                <time class="ferry-time" datetime="PT2H30M">8 min</time>
+                <div class="ferry-info-text">
+                    <span class="ferry-distance">🚶‍♂️ 485 m</span>
+                    <time class="ferry-time" datetime="PT0H4M">~ 4 min</time>
+                </div>
             </div>
             <button id="toggle-nav-menu">
                 <svg id="open-menu-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
