f06cf68a533cfbf79a837405052d9cd2fb04f71e
davidfrp <davidfrp@outlook.dk>
2021-12-30 18:14:32 +0100

    Added copyright notice.

--- website/src/pages/navigationPage.js
+++ website/src/pages/navigationPage.js
@@ -1,6 +1,9 @@
 import "./navigationPage.scss";
 import { createElementFromString } from "../utils/utils";
 
+/**
+ * @copyright Using SVG icons from https://feathericons.com/
+ */
 function NavigationPage(props) {
     const pageElement = createElementFromString(`
         <nav>
@@ -34,11 +37,13 @@ function NavigationPage(props) {
                 </div>
             ` : ""}
             <button id="toggle-nav-menu">
+                <!-- SVG icon from https://feathericons.com/ -->
                 <svg id="open-menu-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <line x1="4" y1="12" x2="20" y2="12"></line>
                     <line x1="4" y1="6" x2="20" y2="6"></line>
                     <line x1="4" y1="18" x2="20" y2="18"></line>
                 </svg>
+                <!-- SVG icon from https://feathericons.com/ -->
                 <svg id="close-menu-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                     <line x1="18" y1="6" x2="6" y2="18"></line>
                     <line x1="6" y1="6" x2="18" y2="18"></line>
